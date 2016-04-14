2fd02e6ae7914ef697e8c7b3a4d353529c144091
Silpion <tom@leftcurlybracket.com>
2016-04-14 03:09:32 +0200

    Initial module

---
+++ app/code/local/LCB/Bootstrap/Block/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Small Magento toolkit for Bootstrap usage
+ *
+ * @category   LCB
+ * @package    LCB_Bootstrap
+ * @license    https://opensource.org/licenses/GPL-3.0 GNU Public License
+ * @author     Silpion Tomasz Gregorczyk <tom@leftcurlybracket.com>
+ */
+class LCB_Bootstrap_Block_Index extends Mage_Core_Block_Template {
+    
+}
